302fe687ea2f64077af2021a922ecb15b91a354f
sss1stclass <sohilkr88@gmail.com>
2024-10-23 14:51:21 +0530

    adding the first commit

---
+++ sohil.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+int  n, arr[20];
+cout << "Enter the no. of Elelments"<<endl;
+cin>>n;
+cout<<"Enter the numbers of Elements"<<endl;
+for(int i = 0; i<n ; i++){
+    cin>>arr[i];
+}
+cout<<"The numbers of elements are "<<endl;
+    cout<<"[";
+for(int i=0; i<n;i++){
+
+    cout<<arr[i]<<" ";
+
+}
+    cout<<"]";
+return 0;
+}
