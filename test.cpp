ae173f4f95fbf6bf475e2d6e1831b8887fb38891
sss1stclass <sohilkr88@gmail.com>
2024-10-28 13:49:50 +0530

    practising stl

--- test.cpp
+++ test.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main (){
-    cout<<"Hello World";
-    return 0;
+int main () {
+   vector<int>v;
+   v.push_back(2);
+   v.push_back(3);
+    v.push_back(45);
+    v.push_back(31);
+
+   cout<<v.size()<<endl;
+   cout<<v.front()<<endl;
+   cout<<v.back()<<endl;
+
+   return 0;
+
 }
