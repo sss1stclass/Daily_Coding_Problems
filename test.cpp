e8207b534ad8db3f5a82baa3323e5d7f064706d5
sss1stclass <sohilkr88@gmail.com>
2024-10-29 07:54:37 +0530

    implemented sorting and 2nd largest element in the array.

--- test.cpp
+++ test.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 int main () {
    vector<int>v;
-   v.push_back(2);
-   v.push_back(3);
-    v.push_back(45);
-    v.push_back(31);
+   v.push_back(10);
+   v.push_back(10);
+//   v.push_back(45);
+//   v.push_back(90);
+//   v.push_back(31);
 
+   for(int i : v ) cout<< i<< " ";
+   cout << endl;
+   sort(v.begin(),v.end());
    cout<<v.size()<<endl;
-   cout<<v.front()<<endl;
-   cout<<v.back()<<endl;
-
+   cout<<v.at(v.size()-2);
    return 0;
 
 }
