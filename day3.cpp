0bef4eb02f69264b97c12bec0de9d5beef0105e1
sss1stclass <sohilkr88@gmail.com>
2024-10-30 08:52:32 +0530

    day3_check_sorted array

--- day3.cpp
+++ day3.cpp
@@ -1,7 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    return 0;
+bool isSorted(vector<int> &arr, int number) {
+    for (int i = 0; i < number - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
+int main() {
+    int arr[] = {1, 4, 6, 9, 0}; // Example array
+    int n = sizeof(arr) / sizeof(arr[0]);
+    vector<int>v(arr,arr+n);
+    int number = v.size();
+
+    int sort = is_sorted(v.begin(), v.end());
+    cout<<sort<<endl;
+
+    if (isSorted(v, number)) {
+        cout << "The array is sorted." << endl;
+    } else {
+        cout << "The array is not sorted." << endl;
+    }
+
+    return 0;
 }
