1d5a94302a4df889a7a002f0b4f1716a5cea5ab0
sss1stclass <sohilkr88@gmail.com>
2024-11-03 07:15:56 +0530

    day4 remove duplicates and count numbers of elm.

--- day4.cpp
+++ day4.cpp
@@ -1,7 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int checkUniqueElement(vector<int> &nums){
+    int count = 0;
+    for(int i =0; i<nums.size(); i++){
+        if(nums[i]==nums[i+1]){
+            count++;
+        }
+    }
+    int elm = nums.size() - count;
+    return  elm;
+}
+
 int main(){
-    cout<<"Hello World";
+   vector<int>v = {1, 1, 2,4,5,6,6,7,7};
+//    set<int>s(v.begin(),v.end());
+//    v.assign(s.begin(), s.end());
+//    cout<< v.size();
+   cout<< checkUniqueElement(v);
+
     return 0;
 }
