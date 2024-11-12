3e41155b547eb8c9f47a3f18f13145aaafad18f5
sss1stclass <sohilkr88@gmail.com>
2024-11-12 12:21:01 +0530

    find the missing number in sorted array

--- day7.cpp
+++ day7.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+int missingNumber(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] + 1 != nums[i + 1]) {
+            return nums[i] + 1;
+        }
+    }
+
+    if (nums[0] != 0) return 0;
+    return nums.size();
+}
+
 int main() {
+    vector<int> nums = {1, 2, 3, 6, 7, 9, 5, 8, 4};
+    cout << missingNumber(nums);
 
     return 0;
 }
