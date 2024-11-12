d6aaa1e26b4d55acd95edc0562e0fbac43624ba5
sss1stclass <sohilkr88@gmail.com>
2024-11-12 13:48:50 +0530

    find consecutive ones probolem solved

--- day8.cpp
+++ day8.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 int findMaxConsecutiveOnes(vector<int> &nums) {
     int count = 0;
+    int maxi = 0;
     for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] && nums[i + 1] == 1) {
+        if (nums[i] == 1) {
             count++;
+//            if (count > max) {
+//                max = count;
+//            }
+            maxi = max(count, maxi);
+        } else {
+            count = 0;
         }
     }
-    return count;
+    return maxi;
 }
 
 int main() {
-    vector<int> nums = {1, 1, 0, 1, 1, 1, 1};
+    vector<int> nums = {1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1};
     cout << findMaxConsecutiveOnes(nums);
     return 0;
 
