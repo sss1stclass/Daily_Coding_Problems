4b50a58ecbc7893e9a341ed8b559271528733c22
sss1stclass <sohilkr88@gmail.com>
2024-11-13 11:15:36 +0530

    day 9 brute force approach

--- day9.cpp
+++ day9.cpp
@@ -5,8 +5,26 @@ using namespace std;
 
 int singleNumber(vector<int> &nums) {
 
+    if (nums.size() == 1) return nums[0];
+    for (int i = 0; i < nums.size(); i++) {
+        int number = nums[i];
+        int count = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] == number) {
+                count++;
+            }
+        }
+        if (count < 2) {
+            return number;
+        }
+    }
+
+
 }
 
 int main() {
+    vector<int> nums = {4, 4, 1, 5, 1, 3, 3};
+    cout << singleNumber(nums);
+
     return 0;
 }
