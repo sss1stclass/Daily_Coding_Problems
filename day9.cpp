4cf941fd78f731f243c145c9ceec55784b5c1dff
sss1stclass <sohilkr88@gmail.com>
2024-11-14 09:09:30 +0530

    day 9 optimal approach

--- day9.cpp
+++ day9.cpp
@@ -4,22 +4,29 @@ using namespace std;
 
 
 int singleNumber(vector<int> &nums) {
+// Brute force approach
 
-    if (nums.size() == 1) return nums[0];
+//    if (nums.size() == 1) return nums[0];
+//    for (int i = 0; i < nums.size(); i++) {
+//        int number = nums[i];
+//        int count = 0;
+//        for (int j = 0; j < nums.size(); j++) {
+//            if (nums[j] == number) {
+//                count++;
+//            }
+//        }
+//        if (count < 2) {
+//            return number;
+//        }
+//    }
+
+// optimal approach
+    int xora = 0;
     for (int i = 0; i < nums.size(); i++) {
-        int number = nums[i];
-        int count = 0;
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] == number) {
-                count++;
-            }
-        }
-        if (count < 2) {
-            return number;
-        }
+        xora = xora ^ nums[i];
     }
 
-
+    return xora;
 }
 
 int main() {
