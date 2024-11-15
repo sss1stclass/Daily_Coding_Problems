0ae38441041bc0fc1210a92fd143b28603c2598a
sss1stclass <sohilkr88@gmail.com>
2024-11-15 18:37:59 +0530

    array and string hashing

--- hash.cpp
+++ hash.cpp
@@ -2,25 +2,47 @@
 
 using namespace std;
 
+// number hashing;
+//int main() {
+//    int n;
+//    cin >> n;
+//    int arr[n];
+//    for (int i = 0; i < n; i++) {
+//        cin >> arr[i];
+//    }
+//    int hash[13] = {0};
+//    for (int i = 0; i < n; i++) {
+//        hash[arr[i]]++;
+//    }
+//    int q;
+//    cin >> q;
+//    while (q--) {
+//        int c;
+//        cin >> c;
+//        cout << hash[c] << endl;
+//    }
+//
+//    return 0;
+//}
+
+// string hashing
+
 int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    string s;
+    cin >> s;
+    int hash[26] = {0};
+    for (int i = 0; i < s.size(); i++) {
+        hash[s[i] - 'a']++;
     }
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]]++;
+    for (auto i: hash) {
+        cout << i << " ";
     }
-    int q;
-    cin >> q;
-    while (q--) {
-        int c;
-        cin >> c;
-        cout << hash[c] << endl;
+    int t;
+    cin >> t;
+    while (t--) {
+        char ch;
+        cin >> ch;
+        cout << hash[ch - 'a'];
     }
-
     return 0;
-
 }
