3478cfa0a48f29545bed124b4c65507733059974
sss1stclass <sohilkr88@gmail.com>
2024-11-16 10:35:59 +0530

    sliding window problem

--- hash.cpp
+++ hash.cpp
@@ -27,6 +27,25 @@ using namespace std;
 
 // string hashing
 
+int maxFrequency(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end());
+
+    long maxFreq = 0;
+    long left = 0;
+    long long sum = 0;
+
+    for (int right = 0; right < nums.size(); ++right) {
+        sum += nums[right];
+        while (nums[right] * (right - left + 1) - sum > k) {
+            sum -= nums[left];
+            left++;
+        }
+        maxFreq = max(maxFreq, right - left + 1);
+    }
+
+    return maxFreq;
+}
+
 int main() {
     string s;
     cin >> s;
