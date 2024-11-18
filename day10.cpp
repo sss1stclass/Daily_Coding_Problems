82fddedd8adcad0769e7e880e440a294bb242d32
sss1stclass <sohilkr88@gmail.com>
2024-11-18 13:17:20 +0530

    brute force approach

--- day10.cpp
+++ day10.cpp
@@ -2,21 +2,21 @@
 
 using namespace std;
 
-int getLongestSubarray(vector<int> &a, long long k) {
-    int n = a.size();
-
-    int len = 0;
-    for (int i = 0; i < n; i++) {
-        long long s = 0;
-        for (int j = i; j < n; j++) {
-            s += a[j];
-            if (s == k)
-                len = max(len, j - i + 1);
-//                len++;
-        }
-    }
-    return len;
-}
+// bruete force approach
+//int getLongestSubarray(vector<int> &a, long long k) {
+//    int n = a.size();
+//
+//    int len = 0;
+//    for (int i = 0; i < n; i++) {
+//        long long s = 0;
+//        for (int j = i; j < n; j++) {
+//            s += a[j];
+//            if (s == k)
+//                len = max(len, j - i + 1);
+//        }
+//    }
+//    return len;
+//}
 
 int main() {
     vector<int> arr = {10, 5, 2, 7, 1, 9, 15};
