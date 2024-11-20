20868624043e7b5efe4822fbfb3a6b48aec91978
sss1stclass <sohilkr88@gmail.com>
2024-11-20 10:44:54 +0530

    solving two sum with optimal and map data structure

--- day12.cpp
+++ day12.cpp
@@ -3,24 +3,38 @@
 
 using namespace std;
 
+//vector<int> twoSum(vector<int> &nums, int target) {
+//    vector<int> temp;
+//    int n = nums.size();
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) {
+//            if (nums[i] + nums[j] == target) {
+//                temp.push_back(i);
+//                temp.push_back(j);
+//                break;
+//            }
+//        }
+//    }
+//    nums = temp;
+//    return nums;
+//}
+
+//using map
+
 vector<int> twoSum(vector<int> &nums, int target) {
-    vector<int> temp;
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] == target) {
-                temp.push_back(i);
-                temp.push_back(j);
-                break;
-            }
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++) {
+        int number = nums[i];
+        int diff = target - number;
+        if (map.find(diff) != map.end()) {
+            return {map[diff], i};
         }
+        map[number] = i;
     }
-    nums = temp;
-    return nums;
 }
 
 int main() {
-    vector<int> nums = {3, 3};
+    vector<int> nums = {1, 6, 1, 1, 4, 2, 3};
     int target = 6;
     vector<int> v = twoSum(nums, target);
     for (auto it: v) {
