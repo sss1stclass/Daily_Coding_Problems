ee5285239cb21c4decdd175b46613174ae9cca98
sss1stclass <sohilkr88@gmail.com>
2024-11-22 10:58:06 +0530

    brute force and optimal

--- day13.cpp
+++ day13.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
+int majorityElement(vector<int> &nums) {
+    int n = nums.size();
+    int hashh[100] = {0};
+    for (int i = 0; i < n; i++) {
+        hashh[nums[i]] += 1;
+    }
+    for (int i = 0; i < 100; i++) {
+        if (hashh[i] > n / 2) {
+            return i;
+        }
+    }
+}
 
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 1, 1, 2, 2};
+    cout << majorityElement(nums);
     return 0;
 }
