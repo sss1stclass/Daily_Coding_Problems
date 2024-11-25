14fa715516d5dfffb8cc9a63ac5a802549e68eea
sss1stclass <sohilkr88@gmail.com>
2024-11-25 11:51:17 +0530

    maximum subarray better and optimal approach

--- day14.cpp
+++ day14.cpp
@@ -2,16 +2,33 @@
 
 using namespace std;
 
+//int maxSubArray(vector<int> &nums) {
+//    int maxi = INT_MIN;
+//    int n = nums.size();
+//    for (int i = 0; i < n; i++) {
+//        int sum = 0;
+//        for (int j = i; j < n; j++) {
+//            cout << nums[i] << "," << nums[j] << endl;
+//            sum += nums[j];
+//            cout << sum << endl;
+//            maxi = max(sum, maxi);
+//        }
+//    }
+//    return maxi;
+//
+//}
+
 int maxSubArray(vector<int> &nums) {
     int maxi = INT_MIN;
     int n = nums.size();
+    int sum = 0;
     for (int i = 0; i < n; i++) {
-        int sum = 0;
-        for (int j = i; j < n; j++) {
-            cout << nums[i] << "," << nums[j] << endl;
-            sum += nums[j];
-            cout << sum << endl;
-            maxi = max(sum, maxi);
+        sum += nums[i];
+        if (sum < 0) {
+            sum = 0;
+        }
+        if (sum > maxi) {
+            maxi = sum;
         }
     }
     return maxi;
@@ -19,7 +36,26 @@ int maxSubArray(vector<int> &nums) {
 }
 
 int main() {
-    vector<int> nums = {4, -1, 2, 1, -2, 1, -3, -5, 4};
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout << maxSubArray(nums);
     return 0;
 }
+
+//class Solution {
+//public:
+//    int maxSubArray(vector<int>& nums) {
+//        int res = nums[0];
+//        int total = 0;
+//
+//        for (int n : nums) {
+//            if (total < 0) {
+//                total = 0;
+//            }
+//
+//            total += n;
+//            res = max(res, total);
+//        }
+//
+//        return res;
+//    }
+//};
