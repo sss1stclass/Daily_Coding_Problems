a9d8ac3b07d5abd3f9a754ccc36e16825a4a1bf9
sss1stclass <sohilkr88@gmail.com>
2024-12-01 10:47:14 +0530

    matrix array better

--- day15.cpp
+++ day15.cpp
@@ -3,49 +3,79 @@
 
 using namespace std;
 
-void markRow(vector<vector<int>> &matrix, int n, int m, int i) {
-    // set all non-zero elements as -1 in the row i:
-    for (int j = 0; j < m; j++) {
-        if (matrix[i][j] != 0) {
-            matrix[i][j] = -1;
-        }
-    }
-}
+//void markRow(vector<vector<int>> &matrix, int n, int m, int i) {
+//    // set all non-zero elements as -1 in the row i:
+//    for (int j = 0; j < m; j++) {
+//        if (matrix[i][j] != 0) {
+//            matrix[i][j] = -1;
+//        }
+//    }
+//}
+//
+//
+//void markCol(vector<vector<int>> &matrix, int n, int m, int j) {
+//    // set all non-zero elements as -1 in the col j:
+//    for (int i = 0; i < n; i++) {
+//        if (matrix[i][j] != 0) {
+//            matrix[i][j] = -1;
+//        }
+//    }
+//}
+//
+//vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+//
+//    // Set -1 for rows and cols
+//    // that contains 0. Don't mark any 0 as -1:
+//
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            if (matrix[i][j] == 0) {
+//                markRow(matrix, n, m, i);
+//                markCol(matrix, n, m, j);
+//            }
+//        }
+//    }
+//
+//    // Finally, mark all -1 as 0:
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            if (matrix[i][j] == -1) {
+//                matrix[i][j] = 0;
+//            }
+//        }
+//    }
+//
+//    return matrix;
+//}
 
 
-void markCol(vector<vector<int>> &matrix, int n, int m, int j) {
-    // set all non-zero elements as -1 in the col j:
-    for (int i = 0; i < n; i++) {
-        if (matrix[i][j] != 0) {
-            matrix[i][j] = -1;
-        }
-    }
-}
 
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
-    // Set -1 for rows and cols
-    // that contains 0. Don't mark any 0 as -1:
-
+void setZeroes(vector<vector<int>> &matrix, int n, int m) {
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (matrix[i][j] == 0) {
-                markRow(matrix, n, m, i);
-                markCol(matrix, n, m, j);
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
-
-    // Finally, mark all -1 as 0:
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == -1) {
+            if (row[i] || col[j]) {
                 matrix[i][j] = 0;
             }
         }
     }
 
-    return matrix;
+    for (auto it: matrix) {
+        for (auto ele: it) {
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
@@ -54,15 +84,7 @@ int main() {
                                   {1, 1, 1}};
     int n = matrix.size();
     int m = matrix[0].size();
-    vector<vector<int>> ans = zeroMatrix(matrix, n, m);
-
-
-    for (auto it: ans) {
-        for (auto ele: it) {
-            cout << ele << " ";
-        }
-        cout << endl;
-    }
+    setZeroes(matrix, n, m);
     return 0;
 }
 
