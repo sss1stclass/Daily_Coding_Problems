9219b02695f250b00c3a6b3a3c510952e8467c2b
sss1stclass <sohilkr88@gmail.com>
2024-12-06 09:59:28 +0530

    pascal tringle

---
+++ day16_PascalTriangle.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> generateRows(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+    for (int i = 1; i <= numRows; i++) {
+        ans.push_back(generateRows(i));
+    }
+    for (auto it: ans) {
+        for (auto i: it) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    return ans;
+}
+
+int main() {
+
+    int row = 5;
+    generate(5);
+    return 0;
+}
