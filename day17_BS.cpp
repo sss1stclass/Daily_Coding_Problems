0fd0a935ebb974086faf15c704cc8a1baaf7007c
sss1stclass <sohilkr88@gmail.com>
2024-12-06 11:32:02 +0530

    bs, lower, upper bound, floor, cril implemented

--- day17_BS.cpp
+++ day17_BS.cpp
@@ -10,9 +10,7 @@ int binarySearch(vector<int> nums, int target) {
     while (low <= high) {
         int mid = (high + low) / 2;
         if (nums[mid] == target) {
-            if (mid > 0) {
-                return mid - 1;
-            } else return 0;
+            return mid;
         } else if (nums[mid] < target) {
             low = mid + 1;
         } else {
@@ -23,11 +21,52 @@ int binarySearch(vector<int> nums, int target) {
 
 }
 
+// lowerBound = smallest index of  arr[index]>=x
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        } else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        } else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
 int main() {
-    vector<int> v = {};
-    int target = ;
-    cout << binarySearch(v, target);
+    vector<int> arr = {3, 5, 9, 15, 19};
+    int n = 5, x = 9;
+//    int ind = upperBound(arr, n, x);
+    int ind = lowerBound(arr, n, x);
+    auto it = lower_bound(arr.begin(), arr.end(), 9);
+    cout << it - arr.begin() << endl;
 
-
+    cout << "The lower bound is the index: " << ind << "\n";
+    //    cout << binarySearch(v, target);
     return 0;
 }
