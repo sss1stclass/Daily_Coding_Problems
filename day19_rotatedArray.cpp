a5a100f369002e05afcd5728205377471eab4516
sss1stclass <sohilkr88@gmail.com>
2024-12-09 10:12:47 +0530

    brute force accepted

--- day19_rotatedArray.cpp
+++ day19_rotatedArray.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+int search(vector<int> &nums, int target) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+
+}
+
 int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    cout << search(nums, target);
 
     return 0;
 }
