ec6d2039abde912ee3dc634e1d9dc47493ddb05e
sss1stclass <sohilkr88@gmail.com>
2024-12-09 11:18:21 +0530

    sorted array 2 for duplicates elements

--- day19_rotatedArray.cpp
+++ day19_rotatedArray.cpp
@@ -9,6 +9,12 @@ int search(vector<int> &nums, int target) {
     while (high >= low) {
         int mid = (low + high) / 2;
         if (nums[mid] == target) return mid;
+        // for the duplicate elements
+//        if(nums[low]==nums[mid] && nums[high]==nums[mid]){
+//            low=low+1;
+//            high = high-1;
+//            continue;
+//        }
         if (nums[mid] >= nums[low]) {
             if (nums[low] <= target && nums[mid] >= target) {
                 high = mid - 1;
@@ -30,7 +36,11 @@ int search(vector<int> &nums, int target) {
 }
 
 int main() {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
+    unordered_set<int> s(nums.begin(), nums.end());
+    for (auto it: s) {
+        cout << it << " ";
+    }
     int target = 0;
     cout << search(nums, target);
 
