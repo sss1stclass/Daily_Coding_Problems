27f37e3b80486aa275417b6ba798cbe4389d2e0e
sss1stclass <sohilkr88@gmail.com>
2024-12-09 10:57:47 +0530

    optimal sol

--- day19_rotatedArray.cpp
+++ day19_rotatedArray.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 int search(vector<int> &nums, int target) {
     int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == target) {
-            return i;
+    int low = 0;
+    int high = n - 1;
+    while (high >= low) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] >= nums[low]) {
+            if (nums[low] <= target && nums[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+            cout << nums[mid] << "less" << endl;
+        } else {
+            if (nums[high] >= target && nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            cout << nums[mid] << "high" << endl;
         }
     }
     return -1;
