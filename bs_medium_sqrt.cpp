104ccc5d7d9e242abcd468d1635b4348845deb80
sss1stclass <sohilkr88@gmail.com>
2024-12-12 21:13:41 +0530

    nthRoot brute and optimal

--- bs_medium_sqrt.cpp
+++ bs_medium_sqrt.cpp
@@ -12,13 +12,13 @@ using namespace std;
 //    return ans;
 //}
 
-int findSQRT(int n) {
+int findSQRT(int n, int m) {
     int low = 1;
     int high = n;
     int ans = 1;
     while (high >= low) {
         int mid = (high + low) / 2;
-        if (mid * mid > n) {
+        if (pow(mid, n) > m) {
             high = mid - 1;
         } else {
             ans = mid;
@@ -28,8 +28,49 @@ int findSQRT(int n) {
     return ans;
 }
 
+int mult(int ini, int n) {
+    int ans = ini;
+    for (int i = 1; i < n; i++) {
+        ans = ans * ini;
+    }
+    return ans;
+}
+
+int nthRoot(int n, int m) {  //brute force
+    int ans = 1;
+    for (int i = 1; i <= m; i++) {
+        if (mult(i, n) == m) {
+            ans = i;
+            break;
+        } else {
+            ans = -1;
+        }
+    }
+    return ans;
+}
+
+int nthRoot(int n, int m) { //optimal
+    int low = 1;
+    int high = m;
+    int ans = 1;
+    while (high >= low) {
+        int mid = (high + low) / 2;
+        if (pow(mid, n) == m) return mid;
+        if (pow(mid, n) > m) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+
 int main() {
-    int num = 5;
-    cout << findSQRT(num);
+    int n = 3;
+    int m = 27;
+//    cout << pow(2, 4) << endl;
+//    cout << nthRoot(n, m);
+    cout << findSQRT(n, m);
     return 0;
 }
