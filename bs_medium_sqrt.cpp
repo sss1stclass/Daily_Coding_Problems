d1f8204cf5761c86b4b27efad946e4a0790e3d68
sss1stclass <sohilkr88@gmail.com>
2024-12-12 19:52:58 +0530

    findSQRT optimal with bs

--- bs_medium_sqrt.cpp
+++ bs_medium_sqrt.cpp
@@ -2,18 +2,34 @@
 
 using namespace std;
 
+//int findSQRT(int n) {
+//    int ans = 1;
+//    for (int i = 0; i <= n; i++) {
+//        if (i * i <= n) {
+//            ans = i;
+//        }
+//    }
+//    return ans;
+//}
+
 int findSQRT(int n) {
+    int low = 1;
+    int high = n;
     int ans = 1;
-    for (int i = 0; i <= n; i++) {
-        if (i * i <= n) {
-            ans = i;
+    while (high >= low) {
+        int mid = (high + low) / 2;
+        if (mid * mid > n) {
+            high = mid - 1;
+        } else {
+            ans = mid;
+            low = mid + 1;
         }
     }
     return ans;
 }
 
 int main() {
-    int num = 28;
+    int num = 5;
     cout << findSQRT(num);
     return 0;
 }
