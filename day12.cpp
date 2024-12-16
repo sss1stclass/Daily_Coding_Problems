0059a2608006859eccc93ea58133b3d018cd1cbb
sss1stclass <sohilkr88@gmail.com>
2024-12-16 11:48:34 +0530

    Three sum optimal

--- day12.cpp
+++ day12.cpp
@@ -33,23 +33,76 @@ vector<int> twoSum(vector<int> &nums, int target) {
     }
 }
 
-vector<vector<int>> threeSum(vector<int> &nums) {
-    int n = nums.size();
-    set<vector<int>> st;
+//vector<vector<int>> threeSum(vector<int> &nums) {
+//    int n = nums.size();
+//    set<vector<int>> st;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = i + 1; j < n; j++) {
+//            for (int k = j + 1; k < n; k++) {
+//                if (nums[i] + nums[j] + nums[k] == 0) {
+//                    vector<int> temp = {nums[i], nums[j], nums[k]};
+//                    sort(temp.begin(), temp.end());
+//                    st.insert(temp);
+//                }
+//            }
+//        }
+//    }
+//    vector<vector<int>> ans(st.begin(), st.end());
+//    return ans;
+//}
+
+//vector<vector<int>> threeSum(vector<int> &nums) {
+//    int n = nums.size();
+//    set<vector<int>> st;
+//    for (int i = 0; i < n; i++) {
+//        set<int> hashMap;
+//        for (int j = i + 1; j < n; j++) {
+//            int k = -(nums[i] + nums[j]);
+//            if (hashMap.find(k) != hashMap.end()) {
+//                vector<int> temp = {nums[i], nums[j], k};
+//                sort(temp.begin(), temp.end());
+//                st.insert(temp);
+//            }
+//            hashMap.insert(nums[j]);
+//        }
+//    }
+//    vector<vector<int>> ans(st.begin(), st.end());
+//    return ans;
+//}
+
+//optimal soln
+
+vector<vector<int>> threeSum(vector<int> &arr) {
+    int n = arr.size();
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
     for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
+            } else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
             }
         }
     }
-    vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
+
 
 int main() {
     vector<int> nums = {-1, 0, 1, 2, -1, -4};
