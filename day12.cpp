7394a875839f6c099a2eeda328196d139e5cec52
sss1stclass <sohilkr88@gmail.com>
2024-12-16 11:09:44 +0530

    Three sum bf

--- day12.cpp
+++ day12.cpp
@@ -33,12 +33,34 @@ vector<int> twoSum(vector<int> &nums, int target) {
     }
 }
 
+vector<vector<int>> threeSum(vector<int> &nums) {
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 int main() {
-    vector<int> nums = {1, 6, 1, 1, 4, 2, 3};
-    int target = 6;
-    vector<int> v = twoSum(nums, target);
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    int target = 0;
+//    vector<int> v = twoSum(nums, target);
+    vector<vector<int>> v = threeSum(nums);
     for (auto it: v) {
-        cout << it << " ";
+        for (auto x: it) {
+            cout << x << " ";
+        }
+        cout << endl;
     }
 
     return 0;
