9b313d66d2c61e9ee42cf2bd42936e53bc1e8c7a
sss1stclass <sohilkr88@gmail.com>
2024-12-19 11:44:06 +0530

    findKthPositive bf apporach 1 O(n)

--- shipping_containers.cpp
+++ shipping_containers.cpp
@@ -88,6 +88,15 @@ int findKthPositive(vector<int> &arr, int k) {
 
 }
 
+//    int findKthPositive(vector<int>& arr, int k) {
+//    for(int i =0;i<arr.size();i++){
+//        if(arr[i]<=k){
+//            k++;
+//        }else break;
+//    }
+//    return k;
+//    }
+
 int main() {
     vector<int> v = {2, 3, 4, 7, 11};
     int days = 5;
