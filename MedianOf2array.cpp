ac4e6991858a43255266d80f25bbfee49e49b0cc
sss1stclass <sohilkr88@gmail.com>
2024-12-22 12:23:26 +0530

    findPages optimal with bs-- O(NlogN)

--- MedianOf2array.cpp
+++ MedianOf2array.cpp
@@ -19,10 +19,43 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
     return -1;
 }
 
+int countPages(vector<int> arr, int pages) {
+    int students = 1;
+    long long studentPages = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] + studentPages <= pages) {
+            studentPages += arr[i];
+        } else {
+            students += 1;
+            studentPages = arr[i];
+        }
+    }
+    return students;
+
+}
+
+int findPages(vector<int> &arr, int n, int m) {
+    if (m > n) return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    while (high >= low) {
+        int mid = (high + low) / 2;
+        int pages = countPages(arr, mid);
+        if (pages > m) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
 int main() {
-    vector<int> nums1 = {1, 2};
-    vector<int> nums2 = {3, 4};
-    double asn = findMedianSortedArrays(nums1, nums2);
+    vector<int> nums1 = {12, 34, 67, 90};
+//    vector<int> nums2 = {3, 4};
+    int n = nums1.size();
+    int m = 2;
+    int asn = findPages(nums1, n, m);
     cout << asn << endl;
     return 0;
 }
