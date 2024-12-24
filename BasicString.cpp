9da4435e6809aaac999ecc28af5a3deb9c38ea4a
sss1stclass <sohilkr88@gmail.com>
2024-12-24 11:17:36 +0530

    string basics

---
+++ BasicString.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string st = "sohil is god";
+    cout << st.length() << endl;
+//    for (int i = st.length(); i >= 0; i--) {
+//        cout << st[i];
+//    }
+//    cout << endl;
+    st.reserve();
+    cout << st;
+    return 0;
+}
+
