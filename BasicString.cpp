1a11ee2a56e41cc35408f177ca6b500b390ad786
sss1stclass <sohilkr88@gmail.com>
2024-12-26 10:37:37 +0530

    largestOddNumber bf

--- BasicString.cpp
+++ BasicString.cpp
@@ -2,15 +2,18 @@
 
 using namespace std;
 
+string largestOddNumber(string num) {
+    for (int i = num.size() - 1; i >= 0; --i) {
+        if ((num[i] - '0') % 2 == 1) {
+            return num.substr(0, i + 1);
+        }
+    }
+    return "";
+}
+
 int main() {
-    string st = "sohil is god";
-    cout << st.length() << endl;
-//    for (int i = st.length(); i >= 0; i--) {
-//        cout << st[i];
-//    }
-//    cout << endl;
-    st.reserve();
-    cout << st;
+    string st = "52";
+    cout << largestOddNumber(st);
     return 0;
 }
 
