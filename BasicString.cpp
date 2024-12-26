723cd8abe8b11c872e291d9bdc40d0e968615d19
sss1stclass <sohilkr88@gmail.com>
2024-12-26 11:34:48 +0530

    isPalindrome bf

--- BasicString.cpp
+++ BasicString.cpp
@@ -11,9 +11,36 @@ string largestOddNumber(string num) {
     return "";
 }
 
+int checkLen(char ch[]) {
+    int len = 0;
+    for (int i = 0; ch[i] != '\0'; i++) {
+        len++;
+    }
+    return len;
+}
+
+bool isPalindrome(char s[]) {
+    // code here
+    int n = strlen(s);
+    int start = 0;
+    int end = n - 1;
+    while (start <= end) {
+        if (s[start] != s[end]) {
+            return false;
+        } else {
+            start++;
+            end--;
+        }
+    }
+    return true;
+}
+
 int main() {
     string st = "52";
+    char star[20] = "abbaaa";
     cout << largestOddNumber(st);
+    cout << isPalindrome(star);
+//    cout << checkLen(star);
     return 0;
 }
 
