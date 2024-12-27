fa6a969e3282a27cdd0fee06a4154310893ea453
sss1stclass <sohilkr88@gmail.com>
2024-12-27 11:38:40 +0530

    A phrase is a palindrome with O(n)

--- BasicString.cpp
+++ BasicString.cpp
@@ -19,9 +19,9 @@ int checkLen(char ch[]) {
     return len;
 }
 
-bool isPalindrome(char s[]) {
+bool checkPalindrome(string s) {
     // code here
-    int n = strlen(s);
+    int n = s.length();
     int start = 0;
     int end = n - 1;
     while (start <= end) {
@@ -35,13 +35,40 @@ bool isPalindrome(char s[]) {
     return true;
 }
 
-bool isssPalindrome(string s) {
+bool valid(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+        return 1;
+    }
+    return 0;
+}
 
+char toLowerCase(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+        return ch;
+    } else {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool isssPalindrome(string s) {
+    string temp = "";
+    for (int i = 0; i <= s.length(); i++) {
+        if (valid(s[i])) {
+            temp.push_back(s[i]);
+        }
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        temp[i] = toLowerCase(temp[i]);
+    }
+    cout << temp << endl;
+    return checkPalindrome(temp);
 }
 
 int main() {
-    string st = "A man, a plan, a canal: Panama";
-    char star[20] = "abbaaa";
+    string st = "A man, a plan, a canal -- Panama";
+    char star[20] = "ab_a";
 //    cout << largestOddNumber(st);
 //    cout << isPalindrome(star);
 //    cout << checkLen(star);
