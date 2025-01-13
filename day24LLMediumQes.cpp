3fb9d70439938baa6954a777b01584980112e5fa
sss1stclass <sohilkr88@gmail.com>
2025-01-13 10:38:21 +0530

    merge two sorted ll in O(nlogn)

--- day24LLMediumQes.cpp
+++ day24LLMediumQes.cpp
@@ -41,13 +41,49 @@ while(temp!= nullptr){
 return head;
 }
 
+Node* mergeTwoLists(Node* list1, Node* list2) {
+    if(list1==NULL && list2 == NULL){
+        return NULL;
+    }
+
+
+    Node* temp = list1;
+    vector<int>v;
+    while(temp!=NULL){
+        v.push_back(temp->data);
+        temp=temp->next;
+    }
+    temp=list2;
+    while(temp!=NULL){
+        v.push_back(temp->data);
+        temp=temp->next;
+    }
+    sort(v.begin(),v.end());
+    Node* newHead = new Node(v[0]);
+    Node* newTemp = newHead;
+    for(int i =1;i<v.size();i++){
+        Node* newNode = new Node(v[i]);
+        newTemp ->next = newNode;
+        newTemp=newNode;
+    }
+    return newHead;
+
+}
+
 int main(){
     Node* head = new Node(2);
     head->next = new Node(9);
     head->next->next= new Node(3);
     head->next->next->next = new Node(1);
     head->next->next->next->next = new Node(5);
-    Node* newHead = sortList(head);
+
+    Node* head2 = new Node(2);
+    head2->next = new Node(8);
+    head2->next->next= new Node(1);
+    head2->next->next->next = new Node(0);
+    head2->next->next->next->next = new Node(5);
+
+    Node* newHead = mergeTwoLists(head,head2);
     print(newHead);
 
     return 0;
