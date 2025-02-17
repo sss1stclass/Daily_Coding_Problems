3c4828cb61602984e284b47a6e83bfbb2ab84f9c
sss1stclass <sohilkr88@gmail.com>
2025-02-17 10:27:46 +0530

    fruit into basket

--- BItManupalation/Revision1.cpp
+++ BItManupalation/Revision1.cpp
@@ -64,14 +64,120 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
     return ans;
 }
 
+//int longestOnes(vector<int>& nums, int k) {
+//    int n = nums.size();
+//    int maxLen = 0;
+//    for( int i  =0;i<n;i++){
+//        int zeros = 0;
+//        for(int j =i;j<n;j++) {
+//            if(nums[j]==0){
+//                zeros++;
+//            }
+//            if(zeros<=k){
+//                int length = j-i+1;
+//                maxLen = max(length, maxLen);
+//            }
+//            else{
+//                break;
+//            }
+//        }
+//    }
+//    return maxLen;
+//
+//}
 
+int longestOnes(vector<int>& nums, int k) {
+    int n = nums.size();
+    int maxLen = 0;
+    int l = 0; int r = 0;
+    int zeros = 0;
+    while(r<n-1){
+        if(nums[r]==0){
+           zeros++;
+        }
+        if(zeros>k){
+            if(nums[l] == 0 ){
+                zeros--;
+            }
+
+
+            l++;
+        }
+        if(zeros<=k){
+            maxLen= max(maxLen, r-l+1);
+        }
+
+        r++;
+    }
+    return maxLen;
+}
+
+int totalFruit(vector<int>& fruits) {
+    int n = fruits.size();
+    unordered_map<int, int>mp;
+    int maxlen = 0;
+    int r = 0; int l = 0;
+    while(r<n){
+        mp[fruits[r]]++;
+        while (mp.size() > 2) {
+            if (mp[fruits[l]] == 0) {
+                mp.erase(fruits[l]);
+            }
+            l++;
+        }
+        if(mp.size()<=2){
+            maxlen= max(maxlen, r-l+1);
+        }
+        r++;
+    }
+    return maxlen;
+}
+
+int characterReplacement(string s, int k) {
+    int n = s.length();
+    unordered_map<char, int> mp;
+    int maxlen = 0;
+    int r = 0;
+    int l = 0;
+
+    while (r < n) {
+        mp[s[r]]++;
+        while (mp.size() > k) {
+            mp[s[l]]--;
+            if (mp[s[l]] == 0) {
+                mp.erase(s[l]);
+            }
+            l++;
+        }
+        maxlen = max(maxlen, r - l + 1);
+        r++;
+    }
+    return maxlen;
+}
+
+string removeDigit(string number, char digit) {
+    int n = number.length();
+    string ans = "";
+    int count =0;
+    for(int i =0;i<n;i++){
+        if(number[i]==digit){
+            count++;
+        }
+    }
+
+    cout<<count;
+    return "as";
+}
 
 
 
 
 int main (){
-   vector<string>s = {"eat","tea","tan","ate","nat","bat"};
-    groupAnagrams(s);
-//cout<<check("eat");
+//   vector<int>s = {3,3,3,1,2,1,1,2,3,3,4};
+     string s = "shrianshi";
+//    cout<<removeDigit(s, '1');
+     string news = s.substr(1);
+     cout<<news;
+
     return 0;
 }
