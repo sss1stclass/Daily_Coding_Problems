537095626eff4d828d32bf2409c554397df99614
sss1stclass <sohilkr88@gmail.com>
2025-03-22 14:51:11 +0530

    zig zag traversal bf

--- Trees/Treezigzagtraversal.cpp
+++ Trees/Treezigzagtraversal.cpp
@@ -44,6 +44,36 @@ vector<vector<int>>printLevelOrder(TreeNode* root){
     return ans;
 }
 
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int>> result;
+    if(root == NULL){
+        return result;
+    }
+    queue<TreeNode*> nodesQueue;
+    nodesQueue.push(root);
+    bool leftToRight = true;
+    while(!nodesQueue.empty()){
+        int size = nodesQueue.size();
+        vector<int> row(size);
+        for(int i = 0; i < size; i++){
+            TreeNode* node = nodesQueue.front();
+            nodesQueue.pop();
+            int index = leftToRight ? i : (size - 1 - i);
+
+            row[index] = node->val;
+            if(node->left){
+                nodesQueue.push(node->left);
+            }
+            if(node->right){
+                nodesQueue.push(node->right);
+            }
+        }
+        leftToRight = !leftToRight;
+        result.push_back(row);
+    }
+    return result;
+}
+
 int main(){
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
@@ -54,7 +84,7 @@ int main(){
     root->right->right = new TreeNode(7);
     printInorder(root);
     cout<<endl;
-    vector<vector<int>>ans = printLevelOrder(root);
+    vector<vector<int>>ans = zigzagLevelOrder(root);
     for(auto i : ans){
         for(auto v : i){
             cout<<v<<" ";
