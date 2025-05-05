d8518c3c64c202990c527d80493e47b683d7dcf3
sss1stclass <sohilkr88@gmail.com>
2025-05-05 13:43:32 +0530

    dfs travel techniques of graph

--- Graphs/BfsDfs.cpp
+++ Graphs/BfsDfs.cpp
@@ -26,7 +26,17 @@ using namespace std;
 
     }
 
-void addEdge(vector <int> adj[], int u, int v) {
+void dfs(int node, vector<int>&ans, int vis[], vector<int> adj[]) {
+        vis[node]=1;
+        ans.push_back(node);
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it, ans, vis, adj);
+            }
+        }
+}
+
+    void addEdge(vector <int> adj[], int u, int v) {
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
@@ -45,7 +55,11 @@ int main()
     addEdge(adj, 1, 2);
     addEdge(adj, 1, 3);
     addEdge(adj, 0, 4);
-    vector <int> ans = bfsOfGraph(5, adj);
+//    vector <int> ans = bfsOfGraph(5, adj);
+vector<int>ans;
+int start = 0;
+int vis[5]={0};
+dfs(start, ans, vis, adj);
     printAns(ans);
 
     return 0;
