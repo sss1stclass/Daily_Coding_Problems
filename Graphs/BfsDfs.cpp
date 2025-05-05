0ede818e2b0a1c49ac4c4ae0f7eb0dc6aa7c230a
sss1stclass <sohilkr88@gmail.com>
2025-05-05 13:32:44 +0530

    bfs travel techniques of graph

---
+++ Graphs/BfsDfs.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        int vis[5] = {0};
+        vis[0]=1;
+        vector<int>bfs;
+        queue<int> q;
+        q.push(0);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+
+
+    }
+
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector <int> &ans) {
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
+int main()
+{
+    vector <int> adj[6];
+
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 0, 4);
+    vector <int> ans = bfsOfGraph(5, adj);
+    printAns(ans);
+
+    return 0;
+}
